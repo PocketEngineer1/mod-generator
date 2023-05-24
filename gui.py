5b29f0c5eead0057f2a093e084371f421d9eeecc
Not Mark <92692104+PocketEngineer1@users.noreply.github.com>
2023-05-24 12:35:34 -0700

    Add files via upload

--- gui.py
+++ gui.py
@@ -7,8 +7,10 @@ import generators.Minetest, generators.Minecraft_Fabric_1_19_3
 
 def GUI(args):
     ui = UI.UI(800, 600, 'Not Mark\'s Mod Generator')
-    ui.parse_xml('ui.xml')
 
+    main_group = UI.ElementGroup('main')
+
+    #region functions
     def Generate_Minetest_Mod():
         if args.mod == None:
             modDef = './mod.json5'
@@ -34,29 +36,18 @@ def GUI(args):
         Log('Started task \'Create Minecraft Fabric 1.19.3 mod\'', 'INFO')
         generators.Minecraft_Fabric_1_19_3.Generate(modData, args)
         Log('Completed task \'Create Minecraft Fabric 1.19.3 mod\'', 'INFO')
-
-    ui.get_element_by_name('main:Button/generate_minetest_mod').click_handler = Generate_Minetest_Mod
-    ui.get_element_by_name('main:Button/generate_minecraft_fabric_1_19_3_mod').click_handler = Generate_Minecraft_Fabric_1_19_3_Mod
-
-    #region RadioButton
-    # radio_buttons = UI.RadioButtonGroup('ButtonGroup')
-
-    # radio_button_1 = UI.RadioButton('main:radio_button/1', 10, 110, button_group=radio_buttons, group=main_group)
-    # radio_button_2 = UI.RadioButton('main:radio_button/2', 10, 140, button_group=radio_buttons, group=main_group)
-    # radio_button_3 = UI.RadioButton('main:radio_button/3', 10, 170, button_group=radio_buttons, group=main_group)
     #endregion
 
-    def ReloadButtonClickHandler():
-        ui.clear_elements()
-        ui.clear_groups()
-        ui.parse_xml('ui.xml')
+    title_text = UI.Text('title_text', 40, 10, 'Not Mark\'s Mod Generator', font_size=80, group=main_group)
+    header_body_seperator = UI.Line('header_body_seperator', (0, 70), (800, 70), thickness=2, group=main_group)
+    generate_minetest_mod_button = UI.Button('generate_minetest_mod_button', 0, 80, 800, 30, click_handler=Generate_Minetest_Mod, text='Generate Minetest Mod', group=main_group)
+    generate_minecraft_fabric_1_19_3_mod = UI.Button('generate_minecraft_fabric_1_19_3_mod', 0, 120, 800, 30, click_handler=Generate_Minecraft_Fabric_1_19_3_Mod, text='Generate Minecraft Fabric 1.19.3 Mod', group=main_group)
 
-        ui.get_element_by_name('main:Button/generate_minetest_mod').click_handeler = Generate_Minetest_Mod
-        ui.get_element_by_name('main:Button/generate_minecraft_fabric_1_19_3_mod').click_handeler = Generate_Minecraft_Fabric_1_19_3_Mod
-
-        ui.add_element(reload_button)
-
-    reload_button = UI.Button('main:button/reload', 0, 570, 90, 30, text='Reload', click_handler=ReloadButtonClickHandler, color=(200,200,200), text_color=(0,0,0))
-    ui.add_element(reload_button)
+    ui.add_element(title_text)
+    ui.add_element(header_body_seperator)
+    ui.add_element(generate_minetest_mod_button)
+    ui.add_element(generate_minecraft_fabric_1_19_3_mod)
+    
+    ui.add_group(main_group)
 
     ui.run()

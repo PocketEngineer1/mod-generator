1666847db7a68aa006111eec0b13d966e0cd8723
Not Mark <92692104+PocketEngineer1@users.noreply.github.com>
2023-05-26 06:23:31 -0700

    Add files via upload

--- gui.py
+++ gui.py
@@ -13,10 +13,6 @@ def GUI(args):
 
     #region Click handelers
     #endregion
-
-    title_text = UI.Text('title_text', 40, 10, 'Not Mark\'s Mod Generator', font_size=80, group=global_group)
-
-    ui.add_element(title_text)
     
     ui.add_group(global_group)
     #endregion
@@ -45,8 +41,8 @@ def GUI(args):
             Log('Completed task \'Create Minecraft Fabric 1.19.3 mod\'', 'INFO')
     #endregion
 
-    generate_minetest_mod = UI.Checkbox('generate_minetest_mod', 10, 110, label='Minetest', group=mod_generation_group)
-    generate_minecraft_fabric_1_19_3_mod = UI.Checkbox('generate_minecraft_fabric_1_19_3_mod', 10, 140, label='Minecraft Fabric 1.19.3', group=mod_generation_group)
+    generate_minetest_mod = UI.Checkbox('generate_minetest_mod', 10, 40, label='Minetest', group=mod_generation_group)
+    generate_minecraft_fabric_1_19_3_mod = UI.Checkbox('generate_minecraft_fabric_1_19_3_mod', 10, 70, label='Minecraft Fabric 1.19.3', group=mod_generation_group)
     generate_button = UI.Button('generate_mods', 0, 570, 800, 30, click_handler=generate_mod, text='Generate Mods', group=mod_generation_group)
 
     ui.add_element(generate_button)
@@ -72,8 +68,8 @@ def GUI(args):
 
     radio_button_group = UI.RadioButtonGroup()
 
-    test_minetest_mod = UI.RadioButton('test_minetest_mod', 10, 110, label='Minetest', button_group=radio_button_group, group=mod_testing_group)
-    test_minecraft_fabric_1_19_3_mod = UI.RadioButton('test_minecraft_fabric_1_19_3_mod', 10, 140, label='Minecraft Fabric 1.19.3', button_group=radio_button_group, group=mod_testing_group)
+    test_minetest_mod = UI.RadioButton('test_minetest_mod', 10, 40, label='Minetest', button_group=radio_button_group, group=mod_testing_group)
+    test_minecraft_fabric_1_19_3_mod = UI.RadioButton('test_minecraft_fabric_1_19_3_mod', 10, 70, label='Minecraft Fabric 1.19.3', button_group=radio_button_group, group=mod_testing_group)
 
     test_button = UI.Button('test_mod', 0, 570, 800, 30, click_handler=test_mod, text='Test Mod', group=mod_testing_group)
 
@@ -89,8 +85,8 @@ def GUI(args):
     tabs = UI.TabGroup()
     tabs.add_group(mod_generation_group)
     tabs.add_group(mod_testing_group)
-    mod_generation_button = UI.Tab('mod_generation_button', 0, 70, 400, 30, tab_group=tabs, group=mod_generation_group, text='Mod Generation', enabled=False, global_group=global_group)
-    mod_testing_button = UI.Tab('mod_testing_button', 400, 70, 400, 30, tab_group=tabs, group=mod_testing_group, text='Mod Testing', global_group=global_group)
+    mod_generation_button = UI.Tab('mod_generation_button', 0, 0, 400, 30, tab_group=tabs, group=mod_generation_group, text='Mod Generation', enabled=False, global_group=global_group)
+    mod_testing_button = UI.Tab('mod_testing_button', 400, 0, 400, 30, tab_group=tabs, group=mod_testing_group, text='Mod Testing', global_group=global_group)
     ui.add_element(mod_generation_button)
     ui.add_element(mod_testing_button)
     #endregion

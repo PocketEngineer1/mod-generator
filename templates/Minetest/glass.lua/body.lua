0dde7bdbdf1688d6a99f8da43dbb786b101b467d
Not Mark <92692104+PocketEngineer1@users.noreply.github.com>
2023-05-29 16:10:28 -0500

    Delete templates/Minetest/glass.lua directory

--- templates/Minetest/glass.lua/body.lua
+++
@@ -1,9 +0,0 @@
-minetest.register_node(name..":!glass.id", {
-	description = "!glass.name",
-	tiles = {"!glass.id.png"},
-	groups = {cracky = 3},
-	stack_max = stack,
-	drawtype = "glasslike",
-	paramtype = "light",
-	sunlight_propagates = true,
-})
